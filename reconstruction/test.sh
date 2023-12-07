051cf405dbf3807e81a71bfaba2cf6a777bb9281
Tungthanhlee <lethanhtung.tung06@gmail.com>
2023-12-07 10:31:35 -0800

    add rec

--- reconstruction/test.sh
+++ reconstruction/test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python reconstruction/reconstruction_test.py --config="reconstruction/config.json" --logdir="logs/" --data_path="dataset/modelnet40_ply_hdf5_2048/"
+# python reconstruction/reconstruction_test.py --config="reconstruction/config.json" --logdir="logs/" --data_path="dataset/modelnet40_ply_hdf5_2048/"
+# python reconstruction/inference.py --config="reconstruction/config.json" --logdir="logs/" --data_path="dataset/modelnet40_ply_hdf5_2048/"
+python reconstruction/inference.py --config="reconstruction/config_shapenet55.json" --logdir="logs/" --data_path="dataset/shapenet_core55/shapenet57448xyzonly.npz"
